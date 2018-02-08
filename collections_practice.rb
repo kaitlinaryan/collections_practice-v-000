b14ab1f1cd1ffb8eaa9c509e967e0c883df7f9d2
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-08 18:04:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+def reverse_array(array)
+array.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+ ###not working
+
+def find_a(string)
+  if string.start_with?("a")
+    return true
+  else
+    false
+end
+
+def sum_array
+end
+
+def add_s
+end
